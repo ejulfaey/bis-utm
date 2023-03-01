3b4b116a5cb110dcbb170e8c82877c1d379f8e09
Dzul <ejulfae@gmail.com>
2023-03-01 14:10:59 +0800

    fix filament asset

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     plugins: [
@@ -15,5 +16,37 @@ export default defineConfig({
                 'app/Forms/Components/**',
             ],
         }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "vendor/filament/filament/dist/app.css",
+                    dest: "../assets",
+                },
+                {
+                    src: "vendor/filament/filament/dist/app.css.map",
+                    dest: "../assets",
+                },
+                {
+                    src: "vendor/filament/filament/dist/app.js",
+                    dest: "../assets",
+                },
+                {
+                    src: "vendor/filament/filament/dist/app.js.map",
+                    dest: "../assets",
+                },
+                {
+                    src: "vendor/filament/filament/dist/echo.js",
+                    dest: "../assets",
+                },
+                {
+                    src: "vendor/filament/filament/dist/echo.js.map",
+                    dest: "../assets",
+                },
+                {
+                    src: "vendor/filament/filament/dist/mix-manifest.json",
+                    dest: "../assets",
+                },
+            ],
+        }),        
     ],
 })
