f0c927fe9920b43e75bc53f59b6cbf81082068de
Dzul <ejulfae@gmail.com>
2023-04-07 01:03:27 +0800

    update inspection chart

--- resources/views/livewire/livewire-chart.blade.php
+++ resources/views/livewire/livewire-chart.blade.php
@@ -2,27 +2,41 @@
 @push('scripts')
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
-    var options = {
-        chart: {
-            type: 'donut'
-        },
-        labels: @json($label),
-        series: @json($data),
-        legend: {
-            show: true,
-            position: 'bottom',
-            height: 50
-        }
-    };
+    init();
 
-    var chart = new ApexCharts(document.querySelector("#{{ $chartId }}"), options);
-    chart.render();
+    function init() {
+        new ApexCharts(document.querySelector("#{{ $chartId }}"), {
+            chart: {
+                type: 'donut'
+            },
+            labels: @json($label),
+            series: @json($data),
+            legend: {
+                show: true,
+                position: 'bottom',
+                height: 50
+            }
+        }).render();
+    }
 
     Livewire.on('refreshLivewireChart', (charts) => {
-        chart.updateOptions({
-            labels: charts['chart-1']['label'],
-            series: charts['chart-1']['data'],
-        });
+        new ApexCharts(document.querySelector("#{{ $chartId }}"), {
+            chart: {
+                type: 'donut'
+            },
+            labels: charts['{{ $chartId }}']['label'],
+            series: charts['{{ $chartId }}']['data'],
+            legend: {
+                show: true,
+                position: 'bottom',
+                height: 50
+            }
+        }).render();
+
+        // chart.updateOptions({
+        //     labels: charts['chart-1']['label'],
+        //     series: charts['chart-1']['data'],
+        // });
     });
 </script>
 @endpush
