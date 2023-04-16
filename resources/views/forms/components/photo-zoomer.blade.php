72ec68cb370c3a4ba4fec9ea2968c7d343624be8
Dzul <ejulfae@gmail.com>
2023-04-16 12:16:17 +0800

    fix photo-zoomer

--- resources/views/forms/components/photo-zoomer.blade.php
+++ resources/views/forms/components/photo-zoomer.blade.php
@@ -1,13 +1,11 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()" :hint="$getHint()" :hint-action="$getHintAction()" :hint-color="$getHintColor()" :hint-icon="$getHintIcon()" :required="$isRequired()" :state-path="$getStatePath()">
     @if($getSrc() !== Storage::url(''))
-    <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}').defer, zoomed: false }" @click.away="zoomed = false" @click="zoomed = !zoomed" class="relative">
-        <img loading="lazy" src="{{ $getSrc() }}" class="p-6 w-auto h-32 object-cover border border-gray-200 rounded-lg shadow">
-        <div x-cloak x-show="zoomed" class="fixed inset-0 bg-gray-200 z-10 flex justify-center items-center" style="padding: 1.5rem;">
-            <div class="max-w-4xl w-full">
-                <p class="font-medium text-center mb-6">Click anywhere to close</span></p>
-                <div class="p-4 bg-white">
-                    <img src="{{ $getSrc() }}" alt="zoom-image" class="w-auto h-full">
-                </div>
+    <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}').defer, zoomed: false }" @click.away="zoomed = false" @click="zoomed = !zoomed" class="relative w-full border p-4 flex justify-center rounded-lg shadow">
+        <img loading="lazy" src="{{ $getSrc() }}" class="rounded" style="height: 32rem">
+        <div x-cloak x-show="zoomed" class="fixed inset-0 bg-gray-100 z-10 flex justify-center items-center">
+            <div class="h-screen">
+                <p class="p-4 font-medium text-center">Click anywhere to close</span></p>
+                <img src="{{ $getSrc() }}" alt="zoom-image" class="object-contain h-full">
             </div>
         </div>
     </div>
